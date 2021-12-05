28349167f651f6bbad3b2f16900aa982d77b7bf9
Sergey <Ivanenkovserg@gmail.com>
2021-12-05 12:33:22 +0300

    Lesson1 insert #1

---
+++ src/com/company/Lesson1.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Lesson1 {
+    public static void main(String[] args) {
+        System.out.print("Hello World!");
+        System.out.println("21");
+        System.out.println("sber");
+    }
+}
